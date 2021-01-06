e2efe473f52f9eedb19fc171c7a82e1c48e4c8d9
Xero64 <xero64@gmail.com>
2021-01-06 16:45:20 +0200

    Added distribution plots and tweaked mesh.

--- gullwing/pyapm/test_gullwing.py
+++ gullwing/pyapm/test_gullwing.py
@@ -6,8 +6,9 @@ from pyapm.output.msh import panelresult_to_msh
 from matplotlib.pyplot import figure
 
 #%% Create Panel System
-jsonfilepath = r'../files/gullwing.json'
+jsonfilepath = 'gullwing.json'
 psys = panelsystem_from_json(jsonfilepath)
+display_markdown(psys)
 
 #%% Assemble and Solve
 psys.assemble_panels()
@@ -15,8 +16,8 @@ psys.assemble_horseshoes()
 psys.solve_system()
 
 #%% Panel Result
-alpha = 0.0
-speed = 82.02815
+alpha = 2.0
+speed = 50.0
 rho = 1.225
 
 pres = PanelResult(f'AoA = {alpha:.1f} degrees', psys)
@@ -31,49 +32,22 @@ display_markdown(pres.surface_loads)
 mshfilepath = psys.name + '.msh'
 panelresult_to_msh(pres, mshfilepath)
 
-#%% Plot Panel Drag
-ypos = []
-drag = []
-yfrc = []
-lift = []
-srfc = psys.srfcs[0]
-for strp in srfc.strps:
-    pnl = strp.pnls[0]
-    grdy = [grd.y for grd in pnl.grds]
-    miny = min(grdy)
-    maxy = max(grdy)
-    rngy = maxy-miny
-    dragval = 0.0
-    yfrcval = 0.0
-    liftval = 0.0
-    for pnl in strp.pnls:
-        dragval += pres.nfres.nffrc[pnl.ind, 0]*pres.acs.dirx
-        yfrcval += pres.nfres.nffrc[pnl.ind, 0]*pres.acs.diry
-        liftval += pres.nfres.nffrc[pnl.ind, 0]*pres.acs.dirz
-    drag.append(dragval/rngy)
-    yfrc.append(yfrcval/rngy)
-    lift.append(liftval/rngy)
-    ypos.append(pnl.pnto.y)
-
-fig = figure(figsize=(12, 8))
-ax = fig.gca()
-ax.plot(ypos, drag)
-ax.set_xlim(-6.0, 0.0)
-ax.set_ylabel('Induced Drag Distribution [N/m]')
-_ = ax.grid(True)
-
-fig = figure(figsize=(12, 8))
-ax = fig.gca()
-ax.plot(ypos, yfrc)
-ax.set_xlim(-6.0, 0.0)
-ax.set_ylabel('Y Force Distribution [N/m]')
-_ = ax.grid(True)
-
-fig = figure(figsize=(12, 8))
-ax = fig.gca()
-ax.plot(ypos, lift)
-ax.set_xlim(-6.0, 0.0)
-ax.set_ylabel('Lift Distribution [N/m]')
-_ = ax.grid(True)
-
-# %%
+#%% Coefficient Distribution Plots
+axt = psys.plot_strip_twist_distribution()
+_ = axt.set_ylabel('Strip Twist [deg]')
+_ = axt.set_xlabel('Span-Wise Coordinate - y [m]')
+axt = psys.plot_strip_chord_distribution()
+_ = axt.set_ylabel('Strip Chord [m]')
+_ = axt.set_xlabel('Span-Wise Coordinate - y [m]')
+axw = psys.plot_strip_width_distribution()
+_ = axw.set_ylabel('Strip Width [m]')
+_ = axw.set_xlabel('Span-Wise Coordinate - y [m]')
+axd = pres.plot_strip_drag_force_distribution(normalise=True)
+_ = axd.set_ylabel('Drag Force Coefficient')
+_ = axd.set_xlabel('Span-Wise Coordinate - y')
+axs = pres.plot_strip_side_force_distribution(normalise=True)
+_ = axs.set_ylabel('Side Force Coefficient')
+_ = axs.set_xlabel('Span-Wise Coordinate - y')
+axl = pres.plot_strip_lift_force_distribution(normalise=True)
+_ = axl.set_ylabel('Lift Force Coefficient')
+_ = axl.set_xlabel('Span-Wise Coordinate - y')
